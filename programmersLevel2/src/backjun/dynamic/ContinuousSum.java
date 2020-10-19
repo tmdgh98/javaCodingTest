72508e3a1be64e7cf013e8b89913585a58b2e8fd
kawksh <duqrlwjstk@naver.com>
2020-10-19 17:50:56 +0900

    학원 20201019

--- programmersLevel2/src/backjun/dynamic/ContinuousSum.java
+++ programmersLevel2/src/backjun/dynamic/ContinuousSum.java
@@ -1,51 +1,27 @@
 package backjun.dynamic;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-//import java.util.Scanner;
+import java.util.Scanner;
 
 public class ContinuousSum {
 	public static void main(String[] args) throws NumberFormatException, IOException {
-//		Scanner sc = new Scanner(System.in);
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		int n = sc.nextInt();
-//		int[][] arr = new int[n][n];
-		int n= Integer.parseInt(br.readLine());
-//		int[] arr= new int[n];
-		String[] arr = br.readLine().split(" ");
-		int max = -1000;
-//		int num = n;
-//		for (int i = 0; i < n; i++) {
-//			arr[i]=sc.nextInt();
-//		}
-		int sum=0;
-		for (int i = 0; i < n; i++) {
-			for (int j = i; j < n; j++) {
-				sum += Integer.parseInt(arr[j]);
-				
-				if(max<sum) {
-					max=sum;
-				}
-				
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		int[] arr= new int[n];
+		arr[0] = sc.nextInt();
+		int max=arr[0];
+		int num=arr[0];
+		for (int i = 1; i < n; i++) {
+			arr[i] = sc.nextInt();
+				num=Math.max(arr[i]+arr[i-1],Math.max(arr[i], arr[i]+num));
+			if(num>=max) {
+				max=num;
 			}
-			sum=0;
+			
 		}
-//		for (int i = 0; i < n; i++) {
-//			for (int j = 0; j < num; j++) {
-//				if(i==0) {
-//					arr[i][j]=sc.nextInt();
-//				}else if(i==1) {
-//					arr[i][j]=arr[i-1][j]+arr[i-1][j+1];
-//				}else {
-//					arr[i][j]=arr[i-1][j]+arr[i-1][j+1]-arr[i-2][j+1];
-//				}
-//				if(max<arr[i][j]) {
-//					max=arr[i][j];
-//				}
-//			}
-//			num--;
-//		}
+		
 		System.out.println(max);
 	}
 }
