58783394fd31dd230f0a5fc9b752d83acc6163e8
tmdgh <duqrlwjstk@naver.com>
2023-08-10 20:20:54 +0900

    230810 ezen

--- programmersLevel2/src/backjun/ezen/Baekjoon14940.java
+++ programmersLevel2/src/backjun/ezen/Baekjoon14940.java
@@ -1,9 +1,13 @@
+package backjun.ezen;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
-public class Main {
+public class Baekjoon14940 {
 
     static class Vo {
         int x;
